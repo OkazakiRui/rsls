b04f61c13a3b8ccc460a810814be812c39a3a70f
OkazakiRui <okazaki200226@gmail.com>
2022-07-07 19:29:03 +0900

    refactor: 綺麗にした

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use anyhow::{anyhow, Result};
 use std::env::current_dir;
 use std::fs::{read_dir, DirEntry};
-use std::io::Error;
 
-fn convert_file_into_string(file: Result<DirEntry, Error>) -> anyhow::Result<String> {
-    let file = file?;
+fn convert_file_into_string(file: DirEntry) -> Result<String> {
     let file_name = match file.file_name().into_string() {
         Ok(name) => name,
-        Err(_) => return Err(anyhow::anyhow!("Could not convert file name into string")),
+        Err(_) => return Err(anyhow!("Could not convert file name into string")),
     };
     match file.file_type()?.is_dir() {
         true => Ok(format!("\x1b[32m{}\x1b[m/", file_name)),
@@ -24,6 +23,13 @@ fn main() {
         }
     };
     for file in files {
+        let file = match file {
+            Ok(file) => file,
+            Err(_) => {
+                println!("Failed to read the file");
+                return;
+            }
+        };
         let file_name = match convert_file_into_string(file) {
             Ok(file_name) => file_name,
             Err(error) => {
