4df6e19681d363c5e88ef9dab2290bbbb7ac0ad7
OkazakiRui <okazaki200226@gmail.com>
2022-07-07 16:39:05 +0900

    feat: file list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env::current_dir;
+use std::fs::read_dir;
+
 fn main() {
-    println!("Hello, world!");
+    let path = current_dir().unwrap();
+    let files = match read_dir(path) {
+        Ok(files) => files,
+        Err(_) => {
+            println!("error reading directory");
+            return;
+        }
+    };
+    for file in files {
+        let file_name = match file.as_ref().unwrap().file_type().unwrap().is_dir() {
+            true => format!(
+                "{}/",
+                file.as_ref()
+                    .expect("error reading file")
+                    .file_name()
+                    .to_str()
+                    .expect("to_strでエラー")
+            ),
+            false => file
+                .expect("error reading file")
+                .file_name()
+                .to_str()
+                .expect("to_strでエラー")
+                .to_string(),
+        };
+        println!("{}", file_name);
+    }
 }
