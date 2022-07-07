a10c967c8a50db2f01397178b33f6d618f9ecc60
OkazakiRui <okazaki200226@gmail.com>
2022-07-07 18:48:55 +0900

    refactor: anyhowを使用し、エラーハンドリングをした

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
 use std::env::current_dir;
-use std::fs::read_dir;
+use std::fs::{read_dir, DirEntry};
+use std::io::Error;
+
+fn convert_file_into_string(file: Result<DirEntry, Error>) -> anyhow::Result<String> {
+    let file = file?;
+    let file_name = match file.file_name().into_string() {
+        Ok(name) => name,
+        Err(_) => return Err(anyhow::anyhow!("Could not convert file name into string")),
+    };
+    match file.file_type()?.is_dir() {
+        true => Ok(format!("{}/", file_name).to_string()),
+        false => Ok(file_name.to_string()),
+    }
+}
 
 fn main() {
-    let path = current_dir().unwrap();
+    let path = current_dir().expect("Could not get current directory");
     let files = match read_dir(path) {
         Ok(files) => files,
         Err(_) => {
-            println!("error reading directory");
+            println!("Failed to read the directory");
             return;
         }
     };
     for file in files {
-        let file_name = match file.as_ref().unwrap().file_type().unwrap().is_dir() {
-            true => format!(
-                "{}/",
-                file.as_ref()
-                    .expect("error reading file")
-                    .file_name()
-                    .to_str()
-                    .expect("to_strでエラー")
-            ),
-            false => file
-                .expect("error reading file")
-                .file_name()
-                .to_str()
-                .expect("to_strでエラー")
-                .to_string(),
+        let file_name = match convert_file_into_string(file) {
+            Ok(file_name) => file_name,
+            Err(error) => {
+                println!("{}", error);
+                return;
+            }
         };
         println!("{}", file_name);
     }
